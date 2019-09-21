075603c692785676b0d317cd7e2be6acad218e4e
Warren Goble <warren@hack.expert>
2019-09-20 23:46:33 -0700

    Using Parcel Bundler with Koa as server

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./components";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { App } from "./components";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App apiKey={process.env.API_KEY} />, document.getElementById("root"));
+
+import Bundler from 'parcel-bundler';
+import Koa from 'koa'
+
+const app = new Koa();
+const bundler = new Bundler('index.pug', {});
+
+app.use(bundler.middleware());
+app.listen(1234);
